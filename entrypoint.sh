982a82e59a0dc3866f17db5a661c63267ca52247
lzh <1216083447@qq.com>
2015-10-02 20:23:51 +0800

    添加vlan_ranges

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,6 +107,8 @@ if [ ! -f /etc/neutron/.complete ];then
     $CRUDINI --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types vxlan
     $CRUDINI --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers openvswitch,l2population
     
+    $CRUDINI --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vlan network_vlan_ranges external:2:2999,private:2:2999
+
     $CRUDINI --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vni_ranges 10:10000
     $CRUDINI --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vxlan_group 224.0.0.1
     
